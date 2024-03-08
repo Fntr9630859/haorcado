3abfcc38eb1d247b6ed24fa88fce65ba55a70056
FNTR3455234 <felipeant06@gmail.com>
2024-03-07 23:46:51 -0600

    Agregar archivos de proyecto.

---
+++ haorcado/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace haorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+            label1.Location = new Point(30, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Puntaje";
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            textBox1.Location = new Point(136, 339);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(56, 52);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Enabled = false;
+            textBox2.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            textBox2.Location = new Point(32, 339);
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.Size = new Size(56, 52);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "0";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            textBox3.Location = new Point(262, 339);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(56, 52);
+            textBox3.TabIndex = 3;
+            textBox3.Text = "0";
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            textBox4.Enabled = false;
+            textBox4.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            textBox4.Location = new Point(32, 140);
+            textBox4.Name = "textBox4";
+            textBox4.ReadOnly = true;
+            textBox4.Size = new Size(113, 52);
+            textBox4.TabIndex = 4;
+            textBox4.Text = "0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            label2.Location = new Point(95, 345);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 46);
+            label2.TabIndex = 5;
+            label2.Text = "*";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 25F, FontStyle.Bold);
+            label3.Location = new Point(212, 341);
+            label3.Name = "label3";
+            label3.Size = new Size(44, 46);
+            label3.TabIndex = 6;
+            label3.Text = "=";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.ForestGreen;
+            button1.Font = new Font("Segoe UI", 20F);
+            button1.Location = new Point(42, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 77);
+            button1.TabIndex = 7;
+            button1.Text = "Empezar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkGreen;
+            button2.Enabled = false;
+            button2.Font = new Font("Segoe UI", 20F);
+            button2.Location = new Point(42, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(172, 77);
+            button2.TabIndex = 8;
+            button2.Text = "Probar";
+            button2.UseVisualStyleBackColor = false;
+            button2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.mau;
+            pictureBox1.Location = new Point(362, 96);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(313, 304);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GrayText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ haorcado/Form1.cs
@@ -0,0 +1,18 @@
+namespace haorcado
+{
+    public partial class Form1 : Form
+    {
+        double resBien = 0;
+        double puntaje = 0;
+        double GameOver = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
